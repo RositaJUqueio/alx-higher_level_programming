8a149a514bbb513b4001ba0b51960dcac510bddd
Rosita J Uqueio <uqueiorose@gmail.com>
2022-06-29 15:24:16 -0700

    my checker hates extra words

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ digit = abs(number) % 10
 if number < 0:
     digit = -digit
 if (digit) > 5:
-    print(F"The Last digit of {number} is {digit} and is greater than 5")
+    print(F"Last digit of {number} is {digit} and is greater than 5")
 elif (digit) == 0:
-    print(F"The Last digit of {number} is {digit} and is 0")   
+    print(F"Last digit of {number} is {digit} and is 0")   
 else:
-    print(F"The Last digit of {number} is {digit} and is less than 6 and not 0")
+    print(F"Last digit of {number} is {digit} and is less than 6 and not 0")
