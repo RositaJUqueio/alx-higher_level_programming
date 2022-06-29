69471c54a99540c937527d9c16e860ea71c37fc4
Rosita J Uqueio <uqueiorose@gmail.com>
2022-06-28 20:44:07 -0700

    nice

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    if chr(a) != "q" and chr(a) != "e":  
-      print("{}".format(chr(a)), end="")
+    if chr(a) != "q" and chr(a) != "e": 
+    print("{}".format(chr(a)), end="")
