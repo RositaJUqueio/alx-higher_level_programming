b9d4f6a4808a82a7f834bacc28a40e76e17517cf
Rosita J Uqueio <uqueiorose@gmail.com>
2022-06-29 20:10:49 -0700

    characters

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz ", end="")
         else:
-            print(f"{n} ", end="")
+            print(f" {n} ", end="")
