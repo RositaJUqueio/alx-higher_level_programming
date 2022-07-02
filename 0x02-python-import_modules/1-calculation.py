b17bd9afc6a89365f98034396dacc55c09fb7aeb
Rosita J Uqueio <uqueiorose@gmail.com>
2022-07-02 10:56:26 -0700

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+# if __name__ == '__main__' checks if a file is imported as a module.
 if __name__ == "__main__":
+    # code won't run if file is imported
 
-    '''a program that imports functions from the file calculator_1.py,
-    does some Maths, and prints the result.
-    Args:
-        a = 10
-        b = 5
-    '''
     from calculator_1 import add, sub, mul, div
-    
+
+    ''' program imports functions from file,
+    does some math and prints result
+    '''
     a = 10
     b = 5
-    
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
