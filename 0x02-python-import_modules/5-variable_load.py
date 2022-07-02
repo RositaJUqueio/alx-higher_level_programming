096d00582078af53ef16f4ccbbc931dd7f4c1018
Rosita J Uqueio <uqueiorose@gmail.com>
2022-07-02 11:55:39 -0700

    Everything can be imported

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from variable_load_5 import a
+    ''' program that imports the variable a
+    and prints its value.'''
+    print("{}".format(a))
+
