dc7149588a6a1f42b28a555223aa27806b43951b
Rosita J Uqueio <uqueiorose@gmail.com>
2022-07-01 20:45:38 -0700

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    '''a program that imports functions from the file calculator_1.py,
+    does some Maths, and prints the result.
+    Args:
+        a = 10
+        b = 5
+    '''
+    from calculator_1 import *
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
