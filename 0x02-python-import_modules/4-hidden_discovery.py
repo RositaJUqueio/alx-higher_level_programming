08838eba4624cbde581282b04afe1ac903da0e23
Rosita J Uqueio <uqueiorose@gmail.com>
2022-07-02 12:25:24 -0700

    hidden trasure??

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
 
     import hidden_4
     names = dir(hidden_4)
+    # dir() is used to find all names defined in hidden_4
     for name in names:
-    if name[:2] != "__":
-        print(name)
+        if name[:2] != "__":
+            print(name)
