dd1852c6966de61d1ed48041bd683e2eecc6aa15
Rosita J Uqueio <uqueiorose@gmail.com>
2022-07-02 09:19:47 -0700

    My first toolbox!

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' program that prints the result of the addition of all arguments
+
+'''
+if __name__ == "__main__":
+    import sys
+
+    result = 0
+    for arg in range(len(sys.argv) - 1):
+
+        return sum(args)
+    print("{}".format(sum(args)))
