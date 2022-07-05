f98199836c5f0da33aed1208c6f3be2dda213b58
Rosita J Uqueio <uqueiorose@gmail.com>
2022-07-05 13:00:01 -0700

    king's affirmation

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order."""
     if my_list:
-        mylist.reverse()
-        for i in my_list:
+        for i in reversed.my_list:
             print("{:d}".format(i))
