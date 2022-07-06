1c5d14ceefd8146cc263b5e423ae06222c96820d
Rosita J Uqueio <uqueiorose@gmail.com>
2022-07-06 10:29:32 -0700

    add infinite

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """program that prints the result of the addition of all arguments 
+    len(sys.argv) , checks how many arguments that have been entered.
+    """
+    import sys
+    result = 0
+
+    for i in range(len(sys.argv) - 1)
+    result += int(sys.argv[a + 1])
+    print("{}".format(result))
