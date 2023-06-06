95e99d97f5d3cb93abf9682dc5965b15592f31f0
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-06 18:00:33 +0000

    fixed minor error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 9):
     for x in range(i + 1, 10):
         if i != x:
             if i == 8 and x == 9:
-                print("{}{}".format(i, x), end="")
+                print("{}{}".format(i, x), end="\n")
             else:
                 print("{}{}".format(i, x), end=", ")
