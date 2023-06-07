f3c9b6b6723bc4b1ab04e734d17e2f4a52731bb7
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-07 17:38:05 +0000

    imports function and prints result

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+if __name__ == "__main__":
+    """program imports function and prints result"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
