66ced2f465ea60442615a36f11b4966b8afe81d9
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-08 12:34:55 -0700

    removed whitespaces

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 
 if __name__ == "__main__":
     import sys
-    
+
     argv = len(sys.argv) - 1
     if argv == 0:
         print("0 arguments.")
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(argv))
     for i in range(argv):
-        print("{}: {}".format(i + 1,sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
