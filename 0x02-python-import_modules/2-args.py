426f3e134d7622131e567018b18ccda977509ec7
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-08 12:30:11 -0700

    corunting arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+if __name__ == "__main__":
+    import sys
+    
+    argv = len(sys.argv) - 1
+    if argv == 0:
+        print("0 arguments.")
+    elif argv == 1:
+        print("{} argument:")
+    else:
+        print("{} arguments:".format(argv))
+    for i in range(argv):
+        print("{}: {}".format(i + 1,sys.argv[i + 1]))
