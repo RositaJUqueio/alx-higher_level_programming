c712d664bafe3a7951fec2fb85e279c3bc5a3eee
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-08 08:17:04 -0700

    added a loop to iterrate through module names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,10 @@
 # Rosita J Uqueio
 
 if __name__ == "__main__":
-    """program prints names defined in module hidden_4.pys"""
+    """program prints names defined in module"""
     import hidden_4
-    x = dir(hidden_4)
-    if x[0] != _ and x[1] != _:
-    print(x)
+
+    names = dir(hidden_4)
+    for n in names:
+        if n[:2] != '__':
+            print(n)
