f4a7c717f86b771363e77388edd69eb01c681c15
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-10 14:50:51 -0700

    small chnages

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 # Rosita J Uqueio
 
 def new_in_list(my_list, idx, element):
-    lists = my_list.copy()
+    lists = list(my_list)
     if idx < 0 or idx >= len(my_list):
         return (lists)
-    
-    lists[idx] = element
-    return (lists)
+    else:
+        lists[idx] = element
+        return (lists)
