cd9b56a09133ef574952d75d3a8b970517077c18
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-12 08:22:27 -0700

    removing white spaces

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,10 @@
 # Rosita J Uqueio
 
 def new_in_list(my_list, idx, element):
+    """function replaces an element in a list at a specific position"""
     my_list = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
 
     my_list[idx] = element
     return my_list
-
