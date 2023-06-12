db15646e5aa454d96e9eb3c7386161a14003790f
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-12 08:43:10 -0700

    removing white spaces

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,9 @@
 # Rosita J Uqueio
 
 def no_c(my_string):
+    """function removes all characters c and C from a string"""
     new_string = ""
     for char in my_string:
-        if char.lower() != 'c':
+        if char != 'c' and char != 'C':
             new_string += char
-    return (new_string)
+    return new_string
