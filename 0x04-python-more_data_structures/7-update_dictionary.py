4b56969b6cb5492908c55845825aaa63568b209c
Rosita J Uqueio <uqueiorose@gmail.com>
2023-06-14 13:59:38 -0700

    Print sorted dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
