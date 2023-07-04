0f012a2e7f85a8d89a8e6744c4a237326047610b
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-03 18:30:44 -0700

    class defines a square & returns area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 # Rosita J Uqueio
 
+"""Define a class sqaure"""
+
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """"""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-            
+
     def area(self):
         """return area of sqaure"""
         return (self.__size ** self.__size)
