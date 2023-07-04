7b3f0ed25ad037425e77bdefa035e735af27e765
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-03 20:43:00 -0700

    sets value for width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """Defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.__width = 0
+        self.width = width  # Use the setter method to set the width
+        self.__height = 0
+        self.height = height  # Use the setter method to set the height
+
+    @property
+    def width(self):
+        """Getter method to retrieve the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method to set the width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method to retrieve the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method to set the height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
