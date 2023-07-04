3942f0a4c5a5deaa2e1989ee62673ea0d85213b8
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-03 17:24:48 -0700

    class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    def __init__(val, size):
+        """creates new square"""
+        val.size = __size
