286ba93e125bc54c975d1b7a93fd8468c810384a
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-06 08:56:25 -0700

    class prevents the user from dynamically creating new instance attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+# Rosita J Uqueio
+"""class prevents user from creating new instance attributes"""
+
+
 class LockedClass:
+    """except if the new instance attribute is called first_name."""
     __slots__ = ('first_name',)
 
     def __setattr__(self, name, value):
         if not hasattr(self, 'first_name') and name != 'first_name':
-            raise AttributeError(f"Cannot create new instance attribute '{name}'")
+            raise AttributeError(f"Cannot create new attribute '{name}'")
         super().__setattr__(name, value)
