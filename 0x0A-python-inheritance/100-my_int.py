5e827ecf8da45a2e6a9774af0eaedba07e92ccc1
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-10 15:43:58 -0700

    function that adds a new attribute to an object if it’s possible

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+"""Defining a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """Overrides == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Overrides != operator with == behavior."""
+        return self.real == value
