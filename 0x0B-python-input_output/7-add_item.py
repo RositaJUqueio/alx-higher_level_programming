cbd8008eff67f97ddfc5d9c605e0c96bec74bf73
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-11 16:53:10 -0700

    making some changes to code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,21 @@
 
 """script that adds all arguments to a Python list"""
 import sys
+import json
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
+
+def load_from_json_file(filename):
+    with open(filename) as file:
+        return json.load(file)
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
