40af167357b62a0a598f2a314f576b72c6b2940c
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-11 16:46:24 -0700

    insertion

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+"""function that inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line 
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    text = ""
+    with open(filename) as n:
+        for line in n:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
