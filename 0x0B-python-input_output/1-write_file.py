396646c6ab9310096d00c2f6772509f88679ba9d
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-11 15:33:04 -0700

    changes to code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 # Rosita J Uqueio
 
-""""Function writes a string to a text file"""
+"""Function writes a string to a text files"""
+
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
-    num_characters = 0
+    """"Returns the number of characters written"""
+    char_count = 0
+
     with open(filename, "w", encoding="utf-8") as file:
         file.write(text)
-        num_characters = len(text)
-    return num_characters
+        char_count = len(text)
 
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
+    return char_count
