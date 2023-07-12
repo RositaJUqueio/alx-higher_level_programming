f1730f7a8bcae638bd798b5314f2b733b7884322
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-11 17:00:39 -0700

    pycodestyle requirements

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,13 +5,16 @@
 import sys
 import json
 
+
 def save_to_json_file(my_obj, filename):
     with open(filename, 'w') as file:
         json.dump(my_obj, file)
 
+
 def load_from_json_file(filename):
     with open(filename) as file:
         return json.load(file)
+
 
 if __name__ == "__main__":
     try:
