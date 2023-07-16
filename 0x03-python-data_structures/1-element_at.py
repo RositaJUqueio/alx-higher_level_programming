1cb153fbd91ce7446bc43dfa759eaaa1321195e6
Rosita J Uqueio <uqueiorose@gmail.com>
2023-07-16 14:20:37 -0700

    refining code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 
 def element_at(my_list, idx):
     """function retrieves an element from a list """
-    if idx < 0 or idx > len(my_list) - 1:
+    if 0 > idx > len(my_list) - 1:
         return None
     return (my_list[idx])
