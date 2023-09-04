e6c3165fac4d3719dc30f89f3f512f6980cceddd
Rosita J uqueio <uqueiorose@gmail.com>
2023-09-04 12:48:41 +0000

    changing docstring number to required value

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 # Rosita J Uqueio
 
+"""
+Function returns the sum of 'a' and 'b'
+Float values are typecasted into integers.
+Raises: TypeError if 'a' or 'b' are not integers.
+"""
+
 def add_integer(a, b=98):
-    """function that adds 2 integers"""
+    """Takes arguments 'a' & 'b'
+    Returns:the sum of arguments
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    a = int(a)
-    b = int(b)
-    return a + b
+    return int(a) + int(b)
