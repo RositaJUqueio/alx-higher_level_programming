6f35f9a5a875c5c245bb08b42804261ff6524e94
Rosita J Uqueio <uqueiorose@gmail.com>
2023-09-11 15:57:16 -0700

    returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+"""Defining a function that takes in an object"""
+
+
+def lookup(obj):
+    """Returns list of available attributes and methods of object"""
+    return dir(obj)
