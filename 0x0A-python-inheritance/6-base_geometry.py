ad4b2f2e3f86a3916c96e3fc9f248fba8499f9f5
Rosita J Uqueio <uqueiorose@gmail.com>
2023-09-11 17:33:30 -0700

    created basegeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+"""Defining a base geometry class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Representing BaseGeometry"""
+    pass
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
