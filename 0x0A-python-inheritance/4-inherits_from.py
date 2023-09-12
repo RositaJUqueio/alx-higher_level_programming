c1d1c357f55430bdc464ee8cdaad632cd4cd4599
Rosita J Uqueio <uqueiorose@gmail.com>
2023-09-11 17:22:32 -0700

    cheks for inheritamce

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Rosita J Uqueio
+
+"""Defining a inheritance-checking function."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return: True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
