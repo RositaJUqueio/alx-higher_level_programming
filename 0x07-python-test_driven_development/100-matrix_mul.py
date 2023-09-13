b91451f80966bad5ea025e7267647302ef29eb2c
Rosita J Uqueio <uqueiorose@gmail.com>
2023-09-13 14:52:12 -0700

    added more test cases

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -4,18 +4,10 @@
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices.
-
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        TypeError: If either m_a or m_b is empty.
-        TypeError: If either m_a or m_b has different-sized rows.
-        ValueError: If m_a and m_b cannot be multiplied.
+    """
+    Multiply two matrices.
     Returns:
-        New matrix.
+        A new matrix representing the multiplication of m_a by m_b.
     """
 
     if m_a == [] or m_a == [[]]:
@@ -48,21 +40,21 @@ def matrix_mul(m_a, m_b):
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    inverted = []
+    inverted_b = []
     for r in range(len(m_b[0])):
-        newRow = []
+        new_row = []
         for c in range(len(m_b)):
-            newRow.append(m_b[c][r])
-        inverted.append(newRow)
+            new_row.append(m_b[c][r])
+        inverted_b.append(new_row)
 
     new_matrix = []
     for row in m_a:
-        newRow = []
-        for col in inverted:
+        new_row = []
+        for col in inverted_b:
             prod = 0
-            for i in range(len(inverted[0])):
+            for i in range(len(inverted_b[0])):
                 prod += row[i] * col[i]
-            newRow.append(prod)
-        new_matrix.append(newRow)
+            new_row.append(prod)
+        new_matrix.append(new_row)
 
     return new_matrix
