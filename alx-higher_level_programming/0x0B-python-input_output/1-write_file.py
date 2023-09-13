87f1cd114b3998ed54ccfda112858fc6fdcd7121
Rosita J Uqueio <uqueiorose@gmail.com>
2023-09-12 20:49:45 -0700

    pycodestyle requirements

--- alx-higher_level_programming/0x0B-python-input_output/1-write_file.py
+++ alx-higher_level_programming/0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,7 @@
 
 def number_of_lines(filename=""):
     """Returns the number of lines in a text file."""
-    line_num= 0
+    line_num = 0
     with open(filename) as f:
         for line_num in f:
             line_num += 1
