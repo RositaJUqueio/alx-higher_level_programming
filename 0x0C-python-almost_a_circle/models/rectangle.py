430c71cdd13e08e06aa0444fb9dc8a661c6096cb
Rosita J Uqueio <uqueiorose@gmail.com>
2023-09-22 16:01:42 -0700

    updated to meet pycodestyle requirements

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,34 +12,54 @@ class Rectangle(Base):
         """Initializing and assigning value attributes.
 
         Args:
-          width (int): The width of new Rectangle.
-          height (int): The height of new Rectangle.
-          x (int): The x coordinate of new Rectangle.
-          y (int): The y coordinate of new Rectangle.
-          id (int): The identity of new Rectangle.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
         """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-        @property
-        def width(self):
-            """Retrieves width value"""
-            return self.__width
+    @property
+    def width(self):
+        """Retrieves width value"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Sets width value"""
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Sets width value"""
+        self.__width = value
 
-        @property
-        def height(self):
-            """Retrieves height value"""
-            return self.__height
+    @property
+    def height(self):
+        """Retrieves height value"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """Sets height value"""
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Sets height value"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Retrieves x value"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x value"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Retrieves y value"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y value"""
+        self.__y = value
