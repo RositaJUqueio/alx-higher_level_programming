2a21a7889082b8eb8f6187165fca3d5533422492
Rosita J Uqueio <uqueiorose@gmail.com>
2023-09-22 15:54:25 -0700

    updated to meet pycodestyle requirements

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,12 +4,13 @@
 """Defines a Rectangle that inherits base class """
 from models.base import Base
 
+
 class Rectangle(Base):
     """Represents a Rectangle"""
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializing and assigning value attributes.
-        
+
         Args:
           width (int): The width of new Rectangle.
           height (int): The height of new Rectangle.
@@ -22,22 +23,22 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-        
+
         @property
         def width(self):
             """Retrieves width value"""
             return self.__width
-        
+
         @width.setter
         def width(self, value):
             """Sets width value"""
             self.__width = value
-            
+
         @property
         def height(self):
             """Retrieves height value"""
             return self.__height
-            
+
         @height.setter
         def height(self, value):
             """Sets height value"""
